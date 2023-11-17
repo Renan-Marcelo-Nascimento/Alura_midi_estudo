bd285c103fb420fb9d94659c96edc70dc3aa7592
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2023-11-16 22:39:33 -0300

    adicionando funções para deixar as teclas funcionais

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
-
+ 
 }
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-
-}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-
-
 while (contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSom;
+    const tecla = listaDeTeclas[contador];
+    //busca o elemento na lista de teclas e atribui a classe que foi informada na lista na posição 1
+    const instrumento = tecla.classList[1];
+    //criamos uma constante para concatenar a stringsom e o resto da classe pra formar o id do Audio  com crase e ${} para variaveis  
+    const idAudio = `#som_${instrumento}`
+    //Pega a lista de tecla em ordem e atribui a função de tocar som para ela
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
     contador = contador + 1;
     
-    console.log(contador);
 }
