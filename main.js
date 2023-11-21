dbabedadefb04b866f9a8c336ee740665de53c19
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2023-11-20 21:54:23 -0300

    adicionandoo primeiras condicionais

--- main.js
+++ main.js
@@ -19,8 +19,12 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
         tocaSom(idAudio);
     }
     
-    tecla.onkeydown = function(){
+    tecla.onkeydown = function(evento){
+
+       
+        if(evento.code === 'Space' || evento.code === 'Enter'){
         tecla.classList.add('ativa');
+        }
     }
 
     tecla.onkeyup = function(){
